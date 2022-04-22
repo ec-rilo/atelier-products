559e684d2758de482b2eb60981365a083fdf844d
Zynh Ludwig <Zynh0722@gmail.com>
2022-04-21 23:46:33 -0700

    extract db name and user in load.sh

--- etl/load.sh
+++ etl/load.sh
@@ -4,17 +4,20 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd "$SCRIPT_DIR/../raw"
 
+DB_USER="postgres"
+DB_NAME="testdb"
+
 echo 'Copying products...'
-psql -U postgres -d testdb -c "COPY products FROM '$SCRIPT_DIR/../raw/product.csv' DELIMITER ',' CSV HEADER;"
+psql -U $DB_USER -d $DB_NAME -c "COPY products FROM '$SCRIPT_DIR/../raw/product.csv' DELIMITER ',' CSV HEADER;"
 
 echo 'Copying features...'
-psql -U postgres -d testdb -c "COPY features FROM '$SCRIPT_DIR/../raw/features.csv' DELIMITER ',' CSV HEADER;"
+psql -U $DB_USER -d $DB_NAME -c "COPY features FROM '$SCRIPT_DIR/../raw/features.csv' DELIMITER ',' CSV HEADER;"
 
 echo 'Copying styles...'
-psql -U postgres -d testdb -c "COPY styles FROM '$SCRIPT_DIR/../raw/styles.csv' DELIMITER ',' CSV HEADER;"
+psql -U $DB_USER -d $DB_NAME -c "COPY styles FROM '$SCRIPT_DIR/../raw/styles.csv' DELIMITER ',' CSV HEADER;"
 
 echo 'Copying photos...'
-psql -U postgres -d testdb -c "COPY photos FROM '$SCRIPT_DIR/../raw/photos.csv' DELIMITER ',' CSV HEADER;"
+psql -U $DB_USER -d $DB_NAME -c "COPY photos FROM '$SCRIPT_DIR/../raw/photos.csv' DELIMITER ',' CSV HEADER;"
 
 echo 'Copying skus...'
-psql -U postgres -d testdb -c "COPY skus FROM '$SCRIPT_DIR/../raw/skus.csv' DELIMITER ',' CSV HEADER;"
+psql -U $DB_USER -d $DB_NAME -c "COPY skus FROM '$SCRIPT_DIR/../raw/skus.csv' DELIMITER ',' CSV HEADER;"
